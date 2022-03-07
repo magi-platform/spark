f8d88b989926a4527d13b8173235226dd988211a
Michael Reynolds <reynoldsm88@gmail.com>
2022-03-07 14:51:20 -0500

    [QOL] - add a flag to toggle debug on spark daemons #1

--- bin/worker.sh
+++ bin/worker.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+if [ "$SPARK_CLUSTER_DEBUG" == true ]; then
+  echo 'starting spark worker with remote debugging enabled...'
+  DEBUG_CONF='-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=5050,suspend=n'
+  export SPARK_WORKER_OPTS="$DEBUG_CONF $SPARK_WORKER_OPTS"
+fi
+
 /opt/spark/sbin/start-worker.sh spark://$SPARK_MASTER_HOST:7077
