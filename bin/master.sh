062aec172a341d52b9a02f3192b703c738b40987
michael reynolds <reynoldsm88@gmail.com>
2022-03-07 14:40:16 -0500

    add cluster debugging flag

--- bin/master.sh
+++ bin/master.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+if [ "$SPARK_CLUSTER_DEBUG" == true ]; then
+  echo 'starting spark master with remote debugging enabled...'
+  DEBUG_CONF='-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=5050,suspend=n'
+  export SPARK_MASTER_OPTS="$DEBUG_CONF $SPARK_MASTER_OPTS"
+fi
+
 /opt/spark/sbin/start-master.sh --host $SPARK_MASTER_HOST
