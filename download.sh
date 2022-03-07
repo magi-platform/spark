21d6f7dbcd7a34b99da9f871a6d7c21637698b34
michael reynolds <reynoldsm88@gmail.com>
2022-03-07 14:48:30 -0500

    [COMPATIBILITY] - downgrade default spark version  3.1.2 due to lack of third party library support for 3.2.x

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if [ ! -f distro/spark.tgz ]; then
-    curl https://archive.apache.org/dist/spark/spark-3.2.0/spark-3.2.0-bin-hadoop2.7.tgz --output distro/spark.tgz
+    curl https://archive.apache.org/dist/spark/spark-3.1.2/spark-3.1.2-bin-hadoop3.2.tgz --output distro/spark.tgz
 fi
